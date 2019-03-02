011b9d8a30dd1a454e5b2da33de310b3e6489677
Oleg Bochkarev <oleg.bochkarev@icloud.com>
2019-03-02 12:34:29 +0400

    Initial Commit

---
+++ GeekbrainsLessons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeekbrainsLessons
+//
+//  Created by Oleg Bochkarev on 02/03/2019.
+//  Copyright © 2019 oleg.bochkarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
