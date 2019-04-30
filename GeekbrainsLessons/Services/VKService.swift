9bc1be4c5fe9a507edb6236aec552d4ca7e408be
Oleg Bochkarev <oleg.bochkarev@icloud.com>
2019-04-30 11:02:14 +0400

    add pods

---
+++ GeekbrainsLessons/Services/VKService.swift
@@ -0,0 +1,13 @@
+//
+//  VKService.swift
+//  GeekbrainsLessons
+//
+//  Created by Oleg Bochkarev on 30/04/2019.
+//  Copyright © 2019 oleg.bochkarev. All rights reserved.
+//
+
+import Foundation
+
+final class VKService {
+    
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '11.0'
+use_frameworks!
+
+def averia_pods
+    #NETWORK
+    pod 'Alamofire'
+end
+
+target 'GeekbrainsLessons' do
+    averia_pods
+end
